a1574c1e12401aedf0c5b70d83e4da72d9de4fdc
Su Anli <sual0001@gmail.com>
2019-09-14 19:12:18 +0800

    feat(game-login-screen): add game code input and start button

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {GameLoginScreen} from './src/screens/GameLoginScreen';
 
 const App = () => {
   return <GameLoginScreen />;
 };
 
-export default App;
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FF5A45',
+  },
+};
+
+export default () => (
+  <PaperProvider theme={theme}>
+    <App />
+  </PaperProvider>
+);
--- src/screens/GameLoginScreen.tsx
+++ src/screens/GameLoginScreen.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  Button as PaperButton,
+  TextInput as PaperTextInput,
+} from 'react-native-paper';
 import styled from 'styled-components/native';
 
 export const GameLoginScreen = () => {
+  const [code, setCode] = useState('');
+
   return (
     <Screen>
       <Wrapper>
@@ -9,6 +15,21 @@ export const GameLoginScreen = () => {
           <Strong>Escape</Strong>
           {'\n'}Game.
         </Title>
+
+        <TextInput
+          label="Enter your game code"
+          value={code}
+          onChangeText={setCode}
+          mode="outlined"
+        />
+
+        <Button
+          uppercase={false}
+          mode="contained"
+          onPress={() => console.log('Pressed')}
+          dark>
+          Start
+        </Button>
       </Wrapper>
     </Screen>
   );
@@ -27,8 +48,15 @@ const Wrapper = styled.View`
 
 const Title = styled.Text`
   font-size: 64;
+  margin-bottom: 50;
 `;
 
 const Strong = styled.Text`
   font-weight: bold;
+`;
+
+const TextInput = styled(PaperTextInput)``;
+
+const Button = styled(PaperButton)`
+  margin-top: 20;
 `;
